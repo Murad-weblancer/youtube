ae65b9561e6f9a90679dbfb6c962f6f81eb16963
Murad <kingjalalov23@gmail.com>
2022-10-21 11:59:24 +0300

    finished commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "e2b8d206fbmsh36f055188eb881ap13ffbfjsn57e1143996d5",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
